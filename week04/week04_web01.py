95bc26730619719137ce5918289015baed1a71f3
2021038 <2021038@daelim.ac.kr>
2024-09-19 10:22:35 +0900

    v0.8 BP2

--- week04/week04_web01.py
+++ week04/week04_web01.py
@@ -8,3 +8,10 @@ values = {'stnId':station_id}
 
 url = api + '?' + str(values)
 print(url)
+
+"""
+지역코드 : 184
+https://www.kma.go.kr/weather/forecast/mid-term-rss3.jsp?{'stnId': '184'}
+
+Process finished with exit code 0
+"""
