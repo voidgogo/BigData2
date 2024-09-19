a65bc982032403efa9ae6acde46ab1d4e5615b65
2021038 <2021038@daelim.ac.kr>
2024-09-19 10:24:58 +0900

    v0.9 BP2

--- week04/week04_web01.py
+++ week04/week04_web01.py
@@ -5,13 +5,12 @@ api = "https://www.kma.go.kr/weather/forecast/mid-term-rss3.jsp"
 
 station_id = input("지역코드 : ")  # 전국 108, 수도권 109, 강원 105, 제주 184 ...
 values = {'stnId':station_id}
-
-url = api + '?' + str(values)
+url = api + '?' + urllib.parse.urlencode(values)
 print(url)
 
 """
 지역코드 : 184
-https://www.kma.go.kr/weather/forecast/mid-term-rss3.jsp?{'stnId': '184'}
+https://www.kma.go.kr/weather/forecast/mid-term-rss3.jsp?stnId=184
 
 Process finished with exit code 0
 """
