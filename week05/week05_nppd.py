f4f36737038a723b3e632dbbd88bb489fc48e955
2021038 <2021038@daelim.ac.kr>
2024-09-19 12:47:44 +0900

    v1.3 BP2

--- week05/week05_nppd.py
+++ week05/week05_nppd.py
@@ -1,21 +1,23 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from sklearn.linear_model import LinearRegression
+#from sklearn.linear_model import LinearRegression
+from sklearn.neighbors import KNeighborsRegressor
 
 lifesat = pd.read_csv("https://github.com/ageron/data/raw/main/lifesat/lifesat.csv")
 X = lifesat[["GDP per capita (USD)"]].values
 y = lifesat[["Life satisfaction"]].values
 
-print(lifesat.head(27))
+# print(lifesat.head(27))
 
-# lifesat.plot(kind='scatter', grid=True, x="GDP per capita (USD)", y="Life satisfaction")
-# plt.axis([23_500, 62_500, 4, 9])
-# plt.show()
-#
-# model = LinearRegression()  # 선형 회귀 모델 적용
-#
-# model.fit(X, y)
-#
-# X_new = [[32_142.0]]  # 대한민국 1인당 GDP 2022년 자료
-# print(model.predict(X_new))
+lifesat.plot(kind='scatter', grid=True, x="GDP per capita (USD)", y="Life satisfaction")
+plt.axis([23_500, 62_500, 4, 9])
+plt.show()
+
+#model = LinearRegression()  # 선형 회귀 모델 적용
+model = KNeighborsRegressor(n_neighbors=3)  # 한국과 1인당 GDP가 가장 가까운 나라 셋
+
+model.fit(X, y)
+
+X_new = [[32_142.0]]  # 대한민국 1인당 GDP 2022년 자료
+print(model.predict(X_new))
