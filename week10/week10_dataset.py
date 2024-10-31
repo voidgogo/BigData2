34631402e7c1518f268249d417e162ca38911584
Daelim Kim <2021038@daelim.ac.kr>
2024-10-31 12:14:31 +0900

    v2.5 BP2

--- week10/week10_dataset.py
+++ week10/week10_dataset.py
@@ -1,6 +1,10 @@
 import seaborn as sns
 import pandas as pd
 
-print(sns.get_dataset_names())
-mpg = sns.load_dataset('mpg')
-print(mpg.head().sort_values('mpg'))
+titanic = sns.load_dataset('titanic')
+# print(titanic.describe())
+# print(titanic.head())
+# print(len(titanic))
+# print(titanic.tail(10))
+print(titanic.info())
+# print(titanic['embarked'], titanic['deck'], titanic['class'])
