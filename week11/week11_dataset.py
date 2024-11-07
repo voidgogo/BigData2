270712538748886125fb13456cf63985dc487096
Daelim Kim <2021038@daelim.ac.kr>
2024-11-07 11:42:47 +0900

    v3.0 BP2

--- week11/week11_dataset.py
+++ week11/week11_dataset.py
@@ -1,24 +1,40 @@
 import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
+from fontTools.subset import subset
 
-# 성별에 따른 생존율 계산
+# 나이에 따른 생존율 계산
 titanic = sns.load_dataset('titanic')
+#print(titanic.info())  # age 칼럼은 177개의 결측값 존재
+# 1) 결측치 행 제거
+titanic_drop_row = titanic.dropna(subset=['age'])
+#print(titanic_drop_row.info())
+# 2) 생존율 계산
+titanic_drop_row['survived'] = titanic_drop_row['survived'].astype(float)
+print(titanic_drop_row['survived'])
+# 3) 시각화
+plt.figure(figsize=(10, 5))
+sns.histplot(data=titanic_drop_row, x='age', weights='survived', bins=8, kde=False)
+plt.title('Survival Rate by Age (Drop NaN rows)')
+plt.xlabel('Age')
+plt.ylabel('Survival Rate (Weighted)')
+plt.show()
+
 # print(titanic['sex'].head())
 # print(titanic.info())
 
 # gender_survival = titanic.groupby(by='sex')['survived'].mean()
 # print(type(gender_survival))  # <class 'pandas.core.series.Series'>
 
-gender_survival = titanic.groupby(by='sex')['survived'].mean().reset_index()
-print(gender_survival)
-#print(type(gender_survival))
-print(gender_survival.info())
-
-sns.barplot(data=gender_survival, x='sex', y='survived')
-plt.title('Survival Rate by Gender')
-plt.ylabel('Survival Rate')
-plt.show()
+# gender_survival = titanic.groupby(by='sex')['survived'].mean().reset_index()
+# print(gender_survival)
+# #print(type(gender_survival))
+# print(gender_survival.info())
+#
+# sns.barplot(data=gender_survival, x='sex', y='survived')
+# plt.title('Survival Rate by Gender')
+# plt.ylabel('Survival Rate')
+# plt.show()
 
 
 
