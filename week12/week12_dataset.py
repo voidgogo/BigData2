d632281898ddb77fbe567a2dd4e62995c42e40ec
Daelim Kim <2021038@daelim.ac.kr>
2024-11-14 12:10:26 +0900

    v3.5 BP2 KNeighborsRegressor

--- week12/week12_dataset.py
+++ week12/week12_dataset.py
@@ -28,7 +28,7 @@ y_pred = model.predict(X_test)  # 검증 셋트를 인수로 예측
 plt.figure(figsize=(5, 2))
 plt.scatter(X_test, y_test, color='blue', label='Real')
 plt.scatter(X_test, y_pred, color='red', label='Predicted')
-plt.title('Linear Regression: Real vs Predicted')
+plt.title('KNeighborsRegressor: Real vs Predicted')
 plt.xlabel('Age')
 plt.ylabel('Survivied')
 plt.show()
