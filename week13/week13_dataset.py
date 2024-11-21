eebb80fdc39fdffa887ef3b7d1209a421097f27d
Daelim Kim <2021038@daelim.ac.kr>
2024-11-21 11:29:22 +0900

    v3.8 BP2

--- week13/week13_dataset.py
+++ week13/week13_dataset.py
@@ -1,12 +1,17 @@
+import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 
-mpg = sns.load_dataset('mpg')
-print(mpg.info())
+mpg = sns.load_dataset('mpg')  # 데이터셋 로딩
+# 데이터 전처리
+mpg.drop(['name'], axis=1, inplace=True)  # 불필요한 칼럼 삭제 (원본 데이터 변경)
 mpg.dropna(inplace=True)  # 결측치가 있는 행 제거 (원본 데이터 변경)
-print(mpg.info())
-print(mpg.head())
-print(mpg.tail())
-print(mpg[['origin']])
-print(mpg.describe())
+mpg = pd.get_dummies(mpg, columns=['origin'], drop_first=True)  # One-hot encoding
+#print(mpg.info())
+
+# 독립변수, 의존변수
+X = mpg.drop(['mpg'], axis=1)  # 레이블 컬럼 제거
+y = mpg['mpg']  # 레이블 (타겟 변수)
+
+#print(X.dtypes)  # 각 열의 데이터 타입 확인
